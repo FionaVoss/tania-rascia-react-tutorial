ddb1b0fe1882801fe03347203459095855fbdd25
Fiona Voss <fiona.voss@redbubble.com>
2019-01-25 10:25:49 -0800

    Add Table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import Table from './Table';
 
 class App extends Component {
     render() {
-        return (
-            <div className="App">
-                <h1>Hello, React!</h1>
-            </div>
-        );
+      return (
+        <div className="container">
+          <Table />
+        </div>
+      );
     }
 }
 
